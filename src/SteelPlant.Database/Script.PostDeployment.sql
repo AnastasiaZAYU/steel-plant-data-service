52563a4ffded782de036215d64e4096702a37e2b
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2026-04-05 18:59:03 +0300

    feat: seed SteelGrades and add sp_FinishBatch procedure
    Added a post-deployment script to seed initial steel grades if missing. Introduced sp_FinishBatch stored procedure to mark batches as finished and log the event. Updated the project file to include these new scripts in the build and deployment process.

---
+++ src/SteelPlant.Database/Script.PostDeployment.sql
@@ -0,0 +1,5 @@
+﻿IF NOT EXISTS (SELECT 1 FROM SteelGrades)
+BEGIN
+	INSERT INTO SteelGrades (GradeName, TargetTemperature) 
+	VALUES ('S235JR', 1600), ('S355J2', 1580), ('30HGSA', 1620);
+END
---
+++ src/SteelPlant.Database/sp_FinishBatch.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[sp_FinishBatch]
+	@BatchId INT,
+	@FinalWeight DECIMAL (10, 2)
+AS
+BEGIN
+	UPDATE SteelBatches
+	SET WeightKG = @FinalWeight,
+		Status = 'Finished'
+	WHERE BatchID = @BatchId
+	
+	INSERT INTO ProductionLogs (BatchID, Message)
+	VALUES (@BatchId, 'Batch finished with weight: ' + CAST(@FinalWeight AS NVARCHAR(20)));
+END
